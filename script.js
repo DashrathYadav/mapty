c97700b32eb3f4339d860769b9a3521bdfd0f17c
Dashrath <sandip9028162859@gmail.com>
2022-01-11 18:29:24 +0530

    map is added  with marker changing feature

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      console.log(position);
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      const arr = [latitude, longitude];
+      const map = L.map('map').setView(arr, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(arr)
+        .addTo(map)
+        .bindPopup('here is the mark where you land.')
+        .openPopup();
+
+      map.on('click', event => {
+        console.log(event);
+        const { lat, lng } = event.latlng;
+        console.log(lat, lng);
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup('here is the mark where you land.')
+          .openPopup();
+      });
+    },
+    error => {
+      log(error);
+    }
+  );
